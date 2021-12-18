4c7a88871938f00043eb7268464d8501891b0dd7
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-12-18 13:00:34 -0300

    images + tsconfig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,21 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
+import hero from "@public/hero.svg";
+
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Head>
+        <title>Recursos EduTech</title>
+      </Head>
+      <div>
+        <h1>Hello</h1>
+        <Image src={hero} alt="Recursos EduTech" />
+      </div>
+    </>
   );
 };
 
